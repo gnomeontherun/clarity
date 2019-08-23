d9d4b55cc402e3f084c34d2641daf639fb52bcf8
Jeremy Wilken <gnomation@gnomeontherun.com>
2019-08-23 14:19:42 -0500

    fix: travis?
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,18 +4,7 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+require('dotenv').config();
 
 module.exports = (on, config) => {
   // This is a simple plugin to capture env variables from the system, since a
